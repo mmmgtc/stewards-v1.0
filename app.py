6f4ca5c2842e78e57d88933334cef0ca0521a0fc
Seth McCullough <seth@endevrs.dev>
2021-12-14 18:49:53 -0500

    Update flask templates

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask.templating import render_template
 app = Flask(__name__, static_folder="assets")
 
 @app.route("/", methods=["GET", "POST"])
+
+
 def main():
-    return render_template("index.html")
+    # Insert API logic here
+    stewards = []
+    return render_template("index.html", stewards=stewards)
