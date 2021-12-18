40e95f746058925b764fa91c8cd9dc3fb3bddefc
Seth McCullough <seth@endevrs.dev>
2021-12-18 16:12:24 -0500

    Fix grid styling

--- app.py
+++ app.py
@@ -3,98 +3,131 @@ import requests
 import pandas as pd
 from flask import Flask
 from flask.templating import render_template
+
 app = Flask(__name__, static_folder="assets")
 
-@app.route("/", methods=["GET", "POST"])
 
+@app.route("/", methods=["GET"])
 def main():
 
-    stewards_data = pd.read_csv('stewards.csv')
+    stewards_data = pd.read_csv("stewards.csv")
     voting_power = []
     json_list = []
 
-    for i in stewards_data['address']:
-        url = 'https://api.boardroom.info/v1/voters/' + i
+    for i in stewards_data["address"]:
+        url = "https://api.boardroom.info/v1/voters/" + i
         r = requests.get(url)
-        if list(r.json().keys())[0] == 'message':
-            url1 = 'https://api.thegraph.com/subgraphs/name/withtally/protocol-gitcoin-bravo-v2'
+        if list(r.json().keys())[0] == "message":
+            url1 = "https://api.thegraph.com/subgraphs/name/withtally/protocol-gitcoin-bravo-v2"
 
             add = i
 
-            CRLF = '\\r\\n'
-            payload='{"query":"query ($voterAddress: String!) {'+CRLF\
-            +'  histories {'+CRLF\
-            +'    totalSupply'+CRLF\
-            +'  },'+CRLF\
-            +'  account(id: $voterAddress) {'+CRLF\
-            +'    id'+CRLF\
-            +'    votes'+CRLF\
-            +'    tokenBalance'+CRLF\
-            +'    ballotsCastCount'+CRLF\
-            +'    proposalsProposedCount'+CRLF\
-            +'    percentageOfTotalVotingPower'+CRLF\
-            +'    frequencyOfParticipationTotal'+CRLF\
-            +'    delegationsCurrentlyReceivedCount'+CRLF\
-            +'    frequencyOfParticipationAsActiveVoter'+CRLF\
-            +'  }'+CRLF\
-            +'  delegators: accounts(orderBy: tokenBalance, orderDirection: desc, where: {delegatingTo: $voterAddress}){'+CRLF\
-            +'    id'+CRLF\
-            +'    votes'+CRLF\
-            +'    tokenBalance'+CRLF\
-            +'    ballotsCastCount'+CRLF\
-            +'    proposalsProposedCount'+CRLF\
-            +'    percentageOfTotalVotingPower'+CRLF\
-            +'    frequencyOfParticipationTotal'+CRLF\
-            +'    delegationsCurrentlyReceivedCount'+CRLF\
-            +'    frequencyOfParticipationAsActiveVoter'+CRLF\
-            +'  }'+CRLF\
-            +'}","variables":{"voterAddress":"'+add+'"}}'
-            headers = {
-            'Content-Type': 'application/json'
-            }
+            CRLF = "\\r\\n"
+            payload = (
+                '{"query":"query ($voterAddress: String!) {'
+                + CRLF
+                + "  histories {"
+                + CRLF
+                + "    totalSupply"
+                + CRLF
+                + "  },"
+                + CRLF
+                + "  account(id: $voterAddress) {"
+                + CRLF
+                + "    id"
+                + CRLF
+                + "    votes"
+                + CRLF
+                + "    tokenBalance"
+                + CRLF
+                + "    ballotsCastCount"
+                + CRLF
+                + "    proposalsProposedCount"
+                + CRLF
+                + "    percentageOfTotalVotingPower"
+                + CRLF
+                + "    frequencyOfParticipationTotal"
+                + CRLF
+                + "    delegationsCurrentlyReceivedCount"
+                + CRLF
+                + "    frequencyOfParticipationAsActiveVoter"
+                + CRLF
+                + "  }"
+                + CRLF
+                + "  delegators: accounts(orderBy: tokenBalance, orderDirection: desc, where: {delegatingTo: $voterAddress}){"
+                + CRLF
+                + "    id"
+                + CRLF
+                + "    votes"
+                + CRLF
+                + "    tokenBalance"
+                + CRLF
+                + "    ballotsCastCount"
+                + CRLF
+                + "    proposalsProposedCount"
+                + CRLF
+                + "    percentageOfTotalVotingPower"
+                + CRLF
+                + "    frequencyOfParticipationTotal"
+                + CRLF
+                + "    delegationsCurrentlyReceivedCount"
+                + CRLF
+                + "    frequencyOfParticipationAsActiveVoter"
+                + CRLF
+                + "  }"
+                + CRLF
+                + '}","variables":{"voterAddress":"'
+                + add
+                + '"}}'
+            )
+            headers = {"Content-Type": "application/json"}
 
-            response = requests.request('POST', url1, headers=headers, data=payload)
+            response = requests.request("POST", url1, headers=headers, data=payload)
             res = json.loads(response.text)
-            if res['data']['account'] == None:
-                voting_power.append('NA')
+            if res["data"]["account"] == None:
+                voting_power.append("NA")
             else:
-                voting_power.append(res['data']['account']['percentageOfTotalVotingPower'])
-            
-        else:
-            voting_power.append((float(r.json()['data']['protocols'][0]['lastCastPower'])/100000000)*100)
+                voting_power.append(
+                    res["data"]["account"]["percentageOfTotalVotingPower"]
+                )
 
-        df_voting_power = pd.DataFrame(voting_power, columns = ['votingweight'])
+        else:
+            voting_power.append(
+                (float(r.json()["data"]["protocols"][0]["lastCastPower"]) / 100000000)
+                * 100
+            )
+
+        df_voting_power = pd.DataFrame(voting_power, columns=["votingweight"])
         result = pd.concat([stewards_data, df_voting_power], axis=1)
 
     w = requests.get("https://api.boardroom.info/v1/protocols/gitcoin")
 
-    totalVotes = w.json()['data']['totalProposals']
+    totalVotes = w.json()["data"]["totalProposals"]
 
     voting_participation = []
 
-
-    for i in range(len(result['address'])):
-        url = 'https://api.boardroom.info/v1/voters/' + result['address'][i]
+    for i in range(len(result["address"])):
+        url = "https://api.boardroom.info/v1/voters/" + result["address"][i]
         r = requests.get(url)
 
-        if str(result['votingweight'][i]) == 'nan':
-            voting_participation.append('NA')
-            
-        elif list(r.json().keys())[0] == 'message':
-                voting_participation.append('NA')    
-        
+        if str(result["votingweight"][i]) == "nan":
+            voting_participation.append("NA")
+
+        elif list(r.json().keys())[0] == "message":
+            voting_participation.append("NA")
+
         else:
-            url = 'https://api.boardroom.info/v1/voters/' + str(result['address'][i])
+            url = "https://api.boardroom.info/v1/voters/" + str(result["address"][i])
             res = requests.get(url)
-            userVotesCast = res.json()['data']['protocols'][0]['totalVotesCast']
-            voting_participation.append((userVotesCast/totalVotes)*100)
+            userVotesCast = res.json()["data"]["protocols"][0]["totalVotesCast"]
+            voting_participation.append((userVotesCast / totalVotes) * 100)
 
-    df2 = pd.DataFrame(voting_participation,columns=['voteparticipation'])
-    df3 = pd.concat([result,df2],axis=1)
+    df2 = pd.DataFrame(voting_participation, columns=["voteparticipation"])
+    df3 = pd.concat([result, df2], axis=1)
 
-    df3['json'] = df3.to_json(orient='records', lines=True).splitlines()
-    for i in range(len(stewards_data['address'])):
-        res = json.loads(df3['json'][i])
+    df3["json"] = df3.to_json(orient="records", lines=True).splitlines()
+    for i in range(len(stewards_data["address"])):
+        res = json.loads(df3["json"][i])
         json_list.append(res)
 
     return render_template("index.html", stewards=json_list)
