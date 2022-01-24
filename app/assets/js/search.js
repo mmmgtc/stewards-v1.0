75fea03f8cf44b8059677cb8c15051ed0285243d
Seth McCullough <seth@endevrs.dev>
2022-01-23 19:16:07 -0500

    Link url query to search input

--- app/assets/js/search.js
+++ app/assets/js/search.js
@@ -1,6 +1,39 @@
-function search_stewards() {
-  let input = document.getElementById("search").value;
-  input = input.toLowerCase();
+let searchParams = new URLSearchParams(window.location.search);
+let search = searchParams.toString().split("=")[1];
+const searchInput = document.getElementById("search");
+
+if (!!search) {
+  searchInput.value = search;
+  filterStewards(search);
+}
+
+searchInput.addEventListener("keyup", function (event) {
+  const search = event.target.value.toLowerCase();
+
+  searchParams.set("search", search);
+
+  if (window.history.replaceState) {
+    const url =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?" +
+      searchParams.toString();
+
+    window.history.replaceState(
+      {
+        path: url,
+      },
+      "",
+      url
+    );
+
+    filterStewards(search);
+  }
+});
+
+function filterStewards(value) {
   let stewards = document.getElementsByClassName("card");
 
   for (i = 0; i < stewards.length; i++) {
@@ -15,10 +48,39 @@ function search_stewards() {
 
     const searchParams = [name, address, workStream].join(" ").toLowerCase();
 
-    if (!searchParams.includes(input)) {
+    if (!searchParams.includes(value)) {
       stewards[i].style.display = "none";
     } else {
       stewards[i].style.display = "list-item";
     }
   }
 }
+
+// function orderStewards() {
+//   orderby = document.getElementById("orderby").value;
+//   direction = document.getElementById("direction").value;
+//   // console.log(orderby, direction)
+
+//   if (orderby == "health") {
+//     window.stewards.sort((a, b) => (a.health < b.health ? 1 : -1));
+//   }
+
+//   if (orderby == "weight") {
+//     window.stewards.sort((a, b) => (a.votingweight < b.votingweight ? 1 : -1));
+//   }
+
+//   if (orderby == "participation") {
+//     window.stewards.sort((a, b) =>
+//       a.participation_snapshot < b.participation_snapshot ? 1 : -1
+//     );
+//   }
+
+//   if (orderby == "posts") {
+//     window.stewards.sort((a, b) => (a.posts < b.posts ? 1 : -1));
+//   }
+
+//   // ascending - from low to high
+//   if (direction == "ascending") {
+//     window.stewards.reverse();
+//   }
+// }
